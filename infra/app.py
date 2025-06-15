4603fd33f3888dc41282e9b4533b996830997987
Lavya Arora <lavyarocket@gmail.com>
2025-06-15 14:46:39 -0700

    minor fix

--- infra/app.py
+++ infra/app.py
@@ -2,13 +2,15 @@
 import os
 from aws_cdk import App, Environment
 from infra_stack.s3_stack import S3Stack
-from infra.infra_stack.fetch_lambda_stack import LambdaStack
+from infra_stack.fetch_lambda_stack import LambdaStack_fetch
+from infra_stack.search_lambda_stack import LambdaStack_search
 
 app = App()
 
 env = Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="us-east-1")
 
 s3_stack = S3Stack(app, "SanctionsS3Stack", env=env)
-lambda_stack = LambdaStack(app, "SanctionsLambdaStack", s3_bucket=s3_stack.bucket, env=env)
+fetch_lambda_stack = LambdaStack_fetch(app, "Fetch_SanctionsLambdaStack", s3_bucket=s3_stack.bucket, env=env)
+search_lambda_stack = LambdaStack_search(app, "Search_SanctionsLambdaStack", s3_bucket=s3_stack.bucket, env=env)
 
 app.synth()
